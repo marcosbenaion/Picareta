43a519c7aaadd430d55201ef0f94100f07d1049d
marcosbenaion <marcosbenaion@gmail.com>
2013-11-28 14:55:38 -0200

    Primeiro upload

---
+++ src/ufpa/br/picareta/Main.java
@@ -0,0 +1,13 @@
+package ufpa.br.picareta;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ufpa/br/picareta/Picareta.java
@@ -0,0 +1,7 @@
+package ufpa.br.picareta;
+
+public class Picareta {
+	
+	// teste
+
+}
